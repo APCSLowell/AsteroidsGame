3a8e692181bfd1310635f55bb6577d94f24ae913
PhilipHuang2 <philiphuang2@gmail.com>
2014-11-05 10:43:08 -0800

    Added new abstract class pixel to house star and sun class

--- AsteroidsGame.pde
+++ AsteroidsGame.pde
@@ -1,14 +1,21 @@
-SpaceShip philip = new SpaceShip();
+private SpaceShip philip = new SpaceShip();
+private Star[] peter = new Star[100];
 
 public void setup() 
 {
   //your code here
   size(400,400);
+  for (int i = 0; i < peter.length; ++i) {
+    peter[i] = new Star();   
+  }
 }
 public void draw() 
 {
   //your code here
   background(0);
+  for (int z = 0; z < peter.length; ++z) {
+    peter[z].show();
+  }
   philip.show();
   philip.move();
 
@@ -17,7 +24,13 @@ public void draw()
 void keyPressed()
 {
   if(keyPressed == true && key == 'h')
+  {
     philip.hyperSpace();
+    for (int i = 0; i < peter.length; ++i) 
+    {
+       peter[i] = new Star();   
+    }
+  }
   if(keyCode == LEFT)
     philip.rotate(-4);
   if(keyCode == RIGHT)
@@ -26,6 +39,52 @@ void keyPressed()
     philip.accelerate(1);
   if(keyCode == DOWN)
     philip.accelerate(-1);
+
+}
+
+interface starSystem
+{
+   public void show();
+}
+
+/*class Sun extends Floater implements starSystem
+{
+  Sun
+  {
+    corners = 8;
+    xCorners = new int [corners];
+    yCorners = new int [corners];
+
+    myColor = ();
+    myCenterX = ;
+    myCenterY = ;
+    myPointDirection =
+  }
+}
+*/
+class Star extends Pixel //implements starSystem
+{
+  Star()
+  {
+    corners = 1;
+    xCorners = new int[corners];
+    yCorners = new int[corners];
+    xCorners[0] = 0; yCorners[0] = 0;
+    myColor = (#FFFFFF);
+    myCenterX = (int)(Math.random() * width);
+    myCenterY = (int)(Math.random() * height);
+  }
+  public void setX(int x){myCenterX = x;}  
+  public int getX(){return (int)myCenterX;}   
+  public void setY(int y){myCenterY = y;}
+  public int getY(){return (int)myCenterY;}
+  public void setDirectionX(double x){myDirectionX = x;}   
+  public double getDirectionX(){return myCenterX;}   
+  public void setDirectionY(double y){myCenterY = y;}   
+  public double getDirectionY(){return myDirectionY;}
+  public void setPointDirection(int degrees){myPointDirection = degrees;}  
+  public double getPointDirection(){return myPointDirection;} 
+  //holds the publics functions move(),accelerate(), rotate(), and show()
 
 }
 
@@ -155,3 +214,40 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 
+abstract class Pixel
+{
+    protected int corners;
+    protected int xCorners[];
+    protected int yCorners[];
+    protected int myColor;
+    protected double myCenterX;
+    protected double myCenterY;
+  abstract public void setX(int x);  
+  abstract public int getX();   
+  abstract public void setY(int y);   
+  abstract public int getY();   
+  abstract public void setDirectionX(double x);   
+  abstract public double getDirectionX();   
+  abstract public void setDirectionY(double y);   
+  abstract public double getDirectionY();   
+  abstract public void setPointDirection(int degrees);   
+  abstract public double getPointDirection(); 
+
+  public void show ()  //Draws the floater at the current position  
+  {             
+    fill(myColor);   
+    stroke(myColor);    
+    //convert degrees to radians for sin and cos         
+    double dRadians = myPointDirection*(Math.PI/180);                 
+    int xRotatedTranslated, yRotatedTranslated;    
+    beginShape();         
+    for(int nI = 0; nI < corners; nI++)    
+    {     
+      //rotate and translate the coordinates of the floater using current direction 
+      xRotatedTranslated = (int)((xCorners[nI]* Math.cos(dRadians)) - (yCorners[nI] * Math.sin(dRadians))+myCenterX);     
+      yRotatedTranslated = (int)((xCorners[nI]* Math.sin(dRadians)) + (yCorners[nI] * Math.cos(dRadians))+myCenterY);      
+      vertex(xRotatedTranslated,yRotatedTranslated);    
+    }   
+    endShape(CLOSE);  
+  }
+}
--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -14,17 +14,24 @@ import java.io.IOException;
 
 public class AsteroidsGame extends PApplet {
 
-SpaceShip philip = new SpaceShip();
+private SpaceShip philip = new SpaceShip();
+private Star[] peter = new Star[100];
 
 public void setup() 
 {
   //your code here
   size(400,400);
+  for (int i = 0; i < peter.length; ++i) {
+    peter[i] = new Star();   
+  }
 }
 public void draw() 
 {
   //your code here
   background(0);
+  for (int z = 0; z < peter.length; ++z) {
+    peter[z].show();
+  }
   philip.show();
   philip.move();
 
@@ -33,7 +40,13 @@ public void draw()
 public void keyPressed()
 {
   if(keyPressed == true && key == 'h')
+  {
     philip.hyperSpace();
+    for (int i = 0; i < peter.length; ++i) 
+    {
+       peter[i] = new Star();   
+    }
+  }
   if(keyCode == LEFT)
     philip.rotate(-4);
   if(keyCode == RIGHT)
@@ -42,6 +55,52 @@ public void keyPressed()
     philip.accelerate(1);
   if(keyCode == DOWN)
     philip.accelerate(-1);
+
+}
+
+interface starSystem
+{
+   public void show();
+}
+
+/*class Sun extends Floater implements starSystem
+{
+  Sun
+  {
+    corners = 8;
+    xCorners = new int [corners];
+    yCorners = new int [corners];
+
+    myColor = ();
+    myCenterX = ;
+    myCenterY = ;
+    myPointDirection =
+  }
+}
+*/
+class Star extends Pixel //implements starSystem
+{
+  Star()
+  {
+    corners = 1;
+    xCorners = new int[corners];
+    yCorners = new int[corners];
+    xCorners[0] = 0; yCorners[0] = 0;
+    myColor = (0xffFFFFFF);
+    myCenterX = (int)(Math.random() * width);
+    myCenterY = (int)(Math.random() * height);
+  }
+  public void setX(int x){myCenterX = x;}  
+  public int getX(){return (int)myCenterX;}   
+  public void setY(int y){myCenterY = y;}
+  public int getY(){return (int)myCenterY;}
+  public void setDirectionX(double x){myDirectionX = x;}   
+  public double getDirectionX(){return myCenterX;}   
+  public void setDirectionY(double y){myCenterY = y;}   
+  public double getDirectionY(){return myDirectionY;}
+  public void setPointDirection(int degrees){myPointDirection = degrees;}  
+  public double getPointDirection(){return myPointDirection;} 
+  //holds the publics functions move(),accelerate(), rotate(), and show()
 
 }
 
@@ -171,6 +230,43 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 
+abstract class Pixel
+{
+    protected int corners;
+    protected int xCorners[];
+    protected int yCorners[];
+    protected int myColor;
+    protected double myCenterX;
+    protected double myCenterY;
+  abstract public void setX(int x);  
+  abstract public int getX();   
+  abstract public void setY(int y);   
+  abstract public int getY();   
+  abstract public void setDirectionX(double x);   
+  abstract public double getDirectionX();   
+  abstract public void setDirectionY(double y);   
+  abstract public double getDirectionY();   
+  abstract public void setPointDirection(int degrees);   
+  abstract public double getPointDirection(); 
+
+  public void show ()  //Draws the floater at the current position  
+  {             
+    fill(myColor);   
+    stroke(myColor);    
+    //convert degrees to radians for sin and cos         
+    double dRadians = myPointDirection*(Math.PI/180);                 
+    int xRotatedTranslated, yRotatedTranslated;    
+    beginShape();         
+    for(int nI = 0; nI < corners; nI++)    
+    {     
+      //rotate and translate the coordinates of the floater using current direction 
+      xRotatedTranslated = (int)((xCorners[nI]* Math.cos(dRadians)) - (yCorners[nI] * Math.sin(dRadians))+myCenterX);     
+      yRotatedTranslated = (int)((xCorners[nI]* Math.sin(dRadians)) + (yCorners[nI] * Math.cos(dRadians))+myCenterY);      
+      vertex(xRotatedTranslated,yRotatedTranslated);    
+    }   
+    endShape(CLOSE);  
+  }
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "AsteroidsGame" };
     if (passedArgs != null) {
